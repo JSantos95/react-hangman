8a2ba15fd39fb6d33d5daa1b50c2567414d313ac
Jose <santosjosel1995@gmail.com>
2020-12-22 15:17:04 -0500

    Moved my API to an env file

--- src/api/getTitles.js
+++ src/api/getTitles.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const getTitles = () => {
-    axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=a5bf2e68ac98505ad392cb7cc10b4ca3&language=en-US&page=1")
-    .then((res) => {
-        const { results } = res;
+    
+    axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
+    .then(({ results }) => {
         return results;
     })
     .then((err) => {
