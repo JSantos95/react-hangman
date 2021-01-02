787324aa73dd69a363b7318221c02ec070f58de7
Jose <santosjosel1995@gmail.com>
2021-01-02 17:06:51 -0500

    Didn't return, or catch, or destructure correctly

--- src/api/dist/getTitles.dev.js
+++ src/api/dist/getTitles.dev.js
@@ -10,10 +10,10 @@ var _axios = _interopRequireDefault(require("axios"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var getTitles = function getTitles() {
-  _axios["default"].get("https://api.themoviedb.org/3/movie/top_rated?api_key=a5bf2e68ac98505ad392cb7cc10b4ca3&language=en-US&page=1").then(function (res) {
-    var results = res.results;
-    return results;
-  }).then(function (err) {
+  return _axios["default"].get("https://api.themoviedb.org/3/movie/top_rated?api_key=".concat(process.env.REACT_APP_API_KEY, "&language=en-US&page=1")).then(function (_ref) {
+    var data = _ref.data;
+    return data;
+  })["catch"](function (err) {
     console.error(err);
   });
 };
--- src/api/getTitles.js
+++ src/api/getTitles.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const getTitles = () => {
-    
-    axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
-    .then(({ results }) => {
-        return results;
-    })
-    .then((err) => {
-        console.error(err);
-    });
+    return axios
+        .get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
+        .then(({ data }) => {
+            return data;
+        })
+        .catch(( err ) => {
+            console.error(err);
+        });
 }
 
 export default getTitles;
