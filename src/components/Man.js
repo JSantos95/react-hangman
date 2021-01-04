cc44f57d7d781876fbd71861236e15537bfca9a6
Jose <santosjosel1995@gmail.com>
2021-01-04 17:18:46 -0500

    Display a stage hangman based on # of stikes

--- src/components/Man.js
+++ src/components/Man.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Man = () => {
-  return(
-      <h1>Hello from Man</h1> 
+const Man = (props) => {
+
+  const [phase, setPhase] = useState('Zero');
+
+  //phase will be rendered whenever props.strike changes 
+  useEffect(() => {
+    switch (props.strikes) {
+      case 1: 
+        setPhase('One');
+        break;
+      case 2: 
+        setPhase('Two');
+        break;
+      case 3:
+        setPhase('Three');
+        break;
+      case 4: 
+        setPhase('Four');
+        break;
+      case 5:
+        setPhase('Five');
+        break;
+      case 6: 
+        setPhase('Six-Game-Over');
+        break;
+      default: 
+        setPhase('Zero');
+    }
+  }, [props.strikes])
+
+  return (
+      <div>
+        <p>Guesses Left: {props.strikes} / 6 </p> 
+        <img src={`/img/${phase}.png`} alt="Hangman" width='500' height='500' />
+      </div>
   )
-  
 }
 
 export default Man;
