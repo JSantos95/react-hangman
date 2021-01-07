798771169722911c60b58d894714a30bdfd792c9
Jose <santosjosel1995@gmail.com>
2021-01-06 20:52:29 -0500

    Styling the movie title to make it more noticable

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,8 +4,8 @@ const Title = (props) => {
   return (
     <div className="title">
       {props.status === 'Winner!' ?
-        <h1 className="title__header">{props.status} {props.title} Was The Correct Movie Title</h1> :
-        <h1 className="title__header">{props.status}, {props.title} Was The Correct Movie Title</h1> 
+        <h1 className="title__header">{props.status} <span className="title__header--sub">{props.title}</span> Was The Correct Movie Title</h1> :
+        <h1 className="title__header">{props.status} <span className="title__header--sub">{props.title}</span> Was The Correct Movie Title</h1> 
       }
       <button className='btn' onClick={() => props.setStart(false)}>New Game</button>
     </div>
